6b33d62c335e64c036b3fa19fa0c7506de5db7cb
Nafis Abdullah Ahmad <n.ahmad.web.cit22@gmail.com>
2024-11-24 10:41:06 +0600

    exception handling for api

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,17 +3,31 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (\Exception $e, Request $request) {
+            if ($request->is('api/*')) {
+                if ($e instanceof NotFoundHttpException) {
+                    return response()->json([
+                        'message' => 'Record not found.'
+                    ], 404);
+                } else {
+                    return response()->json([
+                        'message' => $e->getMessage()
+                    ], 400);
+                }
+            }
+        });
     })->create();
